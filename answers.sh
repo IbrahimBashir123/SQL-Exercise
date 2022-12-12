5e3b616548506422e6cb86398375553251cb095c
Ibrahim Bachir <Ibrahimbashir11111@gmail.com>
2022-12-12 22:25:03 +0200

    Basic Queries

---
+++ answers.sh
@@ -0,0 +1,33 @@
+# ----------------- Basic Queries -----------------------
+
+
+# 1. Get all the names of students in the database 
+SELECT "Name" FROM students;
+
+# 2. Get all the data of students above 30 years old 
+SELECT * FROM students WHERE Age > 30;
+
+# 3. Get the names of the females who are 30 years old 
+SELECT * Name FROM students
+WHERE Gender = "F" AND Age > 30;
+
+# 4. Get the number of Points of Alex 
+
+SELECT * Points FROM students
+WHERE Name = "Alex";
+
+# 5. Add yourself as a new student \(your name, your age...\) 
+
+INSERT INTO students VALUES(7, 'Ibrahim', '21', 'M', '366');
+
+# 6. Increase the points of Basma because she solved a new exercise 
+
+UPDATE students
+SET Points = 500
+WHERE Name = 'Basma';
+
+# 7. Decrease the points of Alex because he's late today 8.
+
+UPDATE students
+SET Points = 150
+WHERE Name = 'Alex';
